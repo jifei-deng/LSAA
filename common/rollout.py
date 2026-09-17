1cff009eceee3766329494e84c5f17db5b61b535
Jifei Deng <75174828+jifei-deng@users.noreply.github.com>
2026-09-17 13:00:57 +0800

    Add files via upload

---
+++ common/rollout.py
@@ -0,0 +1,153 @@
+import numpy as np
+import time
+
+
+class RolloutWorker:
+    def __init__(self, env, agents, args):
+        self.env = env
+        self.agents = agents
+        self.episode_limit = args.episode_limit
+        self.n_actions = args.n_actions
+        self.n_agents = args.n_agents
+        self.state_shape = args.state_shape
+        self.obs_shape = args.obs_shape
+        self.args = args
+
+        self.epsilon = args.epsilon
+        self.anneal_epsilon = args.anneal_epsilon
+        self.min_epsilon = args.min_epsilon
+
+        print('Init RolloutWorker')
+
+    def generate_episode(self, episode_counts=None, episode_num=None, agent_obs=None, ask_budget=None, ans_budget=None, evaluate=False, obs_act=None, gmm_manger=None):
+        if evaluate and episode_num == 0:
+            self.env.close()
+
+        o, u, r, s, avail_u, u_onehot, terminate, padded = [], [], [], [], [], [], [], []
+
+        self.env.reset()
+        terminated = False
+
+        step = 0
+        episode_reward = 0
+        last_action = np.zeros((self.args.n_agents, self.args.n_actions))
+        self.agents.policy.init_hidden(1)
+
+        epsilon = 0 if evaluate else self.epsilon
+        if self.args.epsilon_anneal_scale == 'episode':
+            epsilon = epsilon - self.anneal_epsilon if epsilon > self.min_epsilon else epsilon
+
+        while not terminated and step < self.episode_limit:
+            if self.args.render:
+                time.sleep(0.1)
+                self.env.render()
+
+            obs = self.env.get_obs()
+            state = self.env.get_state()
+            if not evaluate:
+                for i in range(self.n_agents):
+
+                    if tuple(obs[i]) in agent_obs[i]:
+                        agent_obs[i][tuple(obs[i])] = agent_obs[i][tuple(obs[i])] + 1
+                    else:
+                        agent_obs[i][tuple(obs[i])] = 1
+
+            actions, avail_actions, actions_onehot = [], [], []
+
+            for agent_id in range(self.n_agents):
+                avail_action = self.env.get_avail_agent_actions(agent_id)
+
+                action, ask_budget, probs, ans_budget = self.agents.choose_action(obs[agent_id], last_action[agent_id], agent_id,
+                                                                                  avail_action, epsilon, episode_counts, agent_obs,
+                                                                                  obs_act, gmm_manger, ask_budget, ans_budget)
+
+                action_onehot = np.zeros(self.args.n_actions)
+                action_onehot[action] = 1
+                actions.append(int(action))
+                actions_onehot.append(action_onehot)
+                avail_actions.append(avail_action)
+                last_action[agent_id] = action_onehot
+
+                if not evaluate:
+                    if tuple(obs[agent_id]) in obs_act[agent_id]:
+                        obs_act[agent_id][tuple(obs[agent_id])].append(probs)
+                    else:
+                        obs_act[agent_id][tuple(obs[agent_id])] = [probs]
+
+            next_obs, reward, terminated, info = self.env.step(actions)
+
+            terminated = True if False not in terminated else False
+            o.append(obs)
+            s.append(state)
+            u.append(np.reshape(actions, [self.n_agents, 1]))
+            u_onehot.append(actions_onehot)
+            avail_u.append(avail_actions)
+            terminate.append([terminated])
+            padded.append([0.])
+            if self.args.individual_rewards:
+                r.append(reward)
+                episode_reward += sum(reward)
+            else:
+                reward = sum(reward)
+                r.append([reward])
+                episode_reward += reward
+            step += 1
+            if self.args.epsilon_anneal_scale == 'step':
+                epsilon = epsilon - self.anneal_epsilon if epsilon > self.min_epsilon else epsilon
+
+        if self.args.render:
+            time.sleep(0.1)
+            self.env.render()
+
+        obs = self.env.get_obs()
+        state = self.env.get_state()
+        o.append(obs)
+        s.append(state)
+        o_next = o[1:]
+        s_next = s[1:]
+        o = o[:-1]
+        s = s[:-1]
+        avail_actions = []
+        for agent_id in range(self.n_agents):
+            avail_action = self.env.get_avail_agent_actions(agent_id)
+            avail_actions.append(avail_action)
+        avail_u.append(avail_actions)
+        avail_u_next = avail_u[1:]
+        avail_u = avail_u[:-1]
+
+        for i in range(step, self.episode_limit):
+            o.append(np.zeros((self.n_agents, self.obs_shape)))
+            u.append(np.zeros([self.n_agents, 1]))
+            s.append(np.zeros(self.state_shape))
+            if self.args.individual_rewards:
+                r.append(np.zeros(self.n_agents))
+            else:
+                r.append([0.])
+            o_next.append(np.zeros((self.n_agents, self.obs_shape)))
+            s_next.append(np.zeros(self.state_shape))
+            u_onehot.append(np.zeros((self.n_agents, self.n_actions)))
+            avail_u.append(np.zeros((self.n_agents, self.n_actions)))
+            avail_u_next.append(np.zeros((self.n_agents, self.n_actions)))
+            padded.append([1.])
+            terminate.append([1.])
+
+        episode = dict(o=o.copy(),
+                       s=s.copy(),
+                       u=u.copy(),
+                       r=r.copy(),
+                       avail_u=avail_u.copy(),
+                       o_next=o_next.copy(),
+                       s_next=s_next.copy(),
+                       avail_u_next=avail_u_next.copy(),
+                       u_onehot=u_onehot.copy(),
+                       padded=padded.copy(),
+                       terminated=terminate.copy()
+                       )
+        for key in episode.keys():
+            episode[key] = np.array([episode[key]])
+        if not evaluate:
+            self.epsilon = epsilon
+
+        if evaluate and episode_num == self.args.evaluate_episode - 1:
+            self.env.close()
+        return episode, episode_reward, r, step, agent_obs, ask_budget, obs_act, ans_budget
---
+++ ma_gym/envs/utils/draw.py
@@ -0,0 +1,82 @@
+from typing import Union
+
+from PIL import Image, ImageDraw
+
+
+def get_cell_sizes(cell_size: Union[int, list, tuple]):
+    if isinstance(cell_size, int):
+        cell_size_vertical = cell_size
+        cell_size_horizontal = cell_size
+    elif isinstance(cell_size, (tuple, list)) and len(cell_size) == 2:
+        cell_size_vertical, cell_size_horizontal = cell_size
+    else:
+        raise TypeError("`cell_size` must be integer, tuple or list with length two.")
+
+    return cell_size_horizontal, cell_size_vertical
+
+
+def draw_grid(rows, cols, cell_size=50, fill='black', line_color='black'):
+    cell_size_x, cell_size_y = get_cell_sizes(cell_size)
+
+    width = cols * cell_size_x
+    height = rows * cell_size_y
+    image = Image.new(mode='RGB', size=(width, height), color=fill)
+
+    draw = ImageDraw.Draw(image)
+    y_start = 0
+    y_end = image.height
+
+    for x in range(0, image.width, cell_size_x):
+        line = ((x, y_start), (x, y_end))
+        draw.line(line, fill=line_color)
+
+    x = image.width - 1
+    line = ((x, y_start), (x, y_end))
+    draw.line(line, fill=line_color)
+
+    x_start = 0
+    x_end = image.width
+
+    for y in range(0, image.height, cell_size_y):
+        line = ((x_start, y), (x_end, y))
+        draw.line(line, fill=line_color)
+
+    y = image.height - 1
+    line = ((x_start, y), (x_end, y))
+    draw.line(line, fill=line_color)
+
+    del draw
+
+    return image
+
+
+def fill_cell(image, pos, cell_size=None, fill='black', margin=0):
+    assert cell_size is not None and 0 <= margin <= 1
+
+    cell_size_x, cell_size_y = get_cell_sizes(cell_size)
+    col, row = pos
+    row, col = row * cell_size_x, col * cell_size_y
+    margin_x, margin_y = margin * cell_size_x, margin * cell_size_y
+    x, y, x_dash, y_dash = row + margin_x, col + margin_y, row + cell_size_x - margin_x, col + cell_size_y - margin_y
+    ImageDraw.Draw(image).rectangle([(x, y), (x_dash, y_dash)], fill=fill)
+
+
+def write_cell_text(image, text, pos, cell_size=None, fill='black', margin=0):
+    assert cell_size is not None and 0 <= margin <= 1
+
+    cell_size_x, cell_size_y = get_cell_sizes(cell_size)
+    col, row = pos
+    row, col = row * cell_size_x, col * cell_size_y
+    margin_x, margin_y = margin * cell_size_x, margin * cell_size_y
+    x, y = row + margin_x, col + margin_y
+    ImageDraw.Draw(image).text((x, y), text=text, fill=fill)
+
+
+def draw_circle(image, pos, cell_size=50, fill='black', radius=0.3):
+    cell_size_x, cell_size_y = get_cell_sizes(cell_size)
+    col, row = pos
+    row, col = row * cell_size_x, col * cell_size_y
+    gap_x, gap_y = cell_size_x * radius, cell_size_y * radius
+    x, y = row + gap_x, col + gap_y
+    x_dash, y_dash = row + cell_size_x - gap_x, col + cell_size_y - gap_y
+    ImageDraw.Draw(image).ellipse([(x, y), (x_dash, y_dash)], outline=fill, fill=fill)
